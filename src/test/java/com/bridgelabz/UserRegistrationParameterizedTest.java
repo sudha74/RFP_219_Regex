d0ee00035b40e21067bf1d9bb484679c1c5485e9
sudha123 <sudhanaukala1997@gmail.com>
2022-12-05 22:10:44 +0530

    Added:parameterised

---
+++ src/test/java/com/bridgelabz/UserRegistrationParameterizedTest.java
@@ -0,0 +1,42 @@
+package com.bridgelabz;
+import static org.junit.Assert.assertEquals;
+
+import java.util.Arrays;
+import java.util.Collection;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+
+@RunWith(Parameterized.class)
+public class UserRegistrationParameterizedTest {
+
+    private final String email;
+    private final String expectedResult;
+    private UserRegistration read;
+
+    public UserRegistrationParameterizedTest(String email, String expectedResult) {
+        this.email = email;
+        this.expectedResult = expectedResult;
+    }
+
+    @Before
+    public void init() {
+        read = new UserRegistration();
+    }
+
+    @Parameterized.Parameters
+    public static Collection emailInput() {
+        return Arrays
+                .asList(new Object[][] { { "abhi@@yahoo.com", "InValid" }, { "abc.xyz@bl.co.india", "InValid" },
+                        { "abc.xyz$bl.co.in", "InValid" }, { "abc.xyz@bl.co.in", "Valid" } });
+    }
+
+
+    @Test
+    public void testEmail() {
+        assertEquals(expectedResult, read.validEmailId(email));
+    }
+
+}
